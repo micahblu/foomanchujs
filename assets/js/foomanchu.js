dd386ff4392c87c795f7eb619f29cc39adc9a255
Micah Blu <micahblu@gmail.com>
2014-03-13 09:44:43 -0700

    Added escape tag feature, will now escape any tag with an extra bracket, so 3 in total to escape

--- assets/js/foomanchu.js
+++ assets/js/foomanchu.js
@@ -32,7 +32,7 @@
 
 			for(var i = 0; i <  ifmatches.length; i++){
 
-				var parts = tmpl.match(/\[{2}#if(.[^\]]+)\]\](.*)\[{2}\/if\]{2}/mi);
+				var parts = tmpl.match(/\[{2}[^\[]#if(.[^\]]+)\]\](.*)\[{2}\/if\]{2}[^\]]/mi);
 
 				var condition = parts[1];
 				var contents = parts[2];
@@ -52,17 +52,36 @@
 		}
 
 		var tags = tmpl.match(/\[{2}\s?(.[^\[]+)\s?\]{2}/gmi);
+		var escape = ''; // placeholder for escaped tags
 
 		for(key in tags){
-
+			// Strip escaped tags & wrap in script tags, to be replaced later on
+			if(tags[key].match(/\[{3}\s?(.[^\[]+)\s?\]{3}/)){
+				escape = tags[key].replace(/[\[\]]/gm,'');
+				tmpl = tmpl.replace(tags[key], "<script type='text/x-foomanchu'>" + escape + "</script>");
+				//console.log(escape);
+			}
 			tag = tags[key].replace(/[\[\]]/gm,'');
-
+			//sconsole.log(tag);
 			for(var symbol in obj){
 				if(tag == symbol){
-					console.log(tag + " = " + obj[symbol]);
+					//console.log(tag + " = " + obj[symbol]);
 					tmpl = tmpl.replace(tags[key], obj[symbol]);
 				}
-			}
+			}	
+		}
+
+		// Replace script tags from escaped template tags
+		var escaped = tmpl.match(/<script\stype=\'text\/x\-foomanchu\'>(.+)<\/script>/gmi);
+		var tag = '';
+		for(key in escaped){
+			//console.log(escaped[tag]);
+			console.log(escaped[key]);
+			tag = escaped[key].replace(/<script\stype=\'text\/x\-foomanchu\'>/, '[[');
+			tag = tag.replace(/<\/script>/, ']]');
+			console.log(tag);
+
+			tmpl = tmpl.replace(escaped[key], tag);
 		}
 
 		return tmpl;
